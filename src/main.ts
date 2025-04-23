f8464a691abc5a863a68009303cf9d5073a436e3
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-04-23 10:37:40 +0500

    new crud tutorial started

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors(); // Optional if you're connecting from frontend
+  await app.listen(3000);
 }
 bootstrap();
